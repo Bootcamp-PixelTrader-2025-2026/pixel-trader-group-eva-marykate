8f2ccf7d4f5e5214aaad1d68d723dc961062d7bd
Celena L'entete <claire.lentete@outlook.com>
2026-01-14 20:35:59 +0100

    Create db and tables

--- csv-cleaner/csvtojson.js
+++
@@ -1,9 +0,0 @@
-const csv = require('csvtojson');
-const fs = require('fs');
-
-csv()
-  .fromFile('stock_cleaned.csv')
-  .then(jsonObj => {
-    fs.writeFileSync('stock.json', JSON.stringify(jsonObj, null, 2));
-    console.log('CSV converted to JSON successfully!');
-  });
